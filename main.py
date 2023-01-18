8818bcc23d672b899c3bf11324e878c940f6c35d
Soloaire <random99322@gmail.com>
2023-01-19 01:13:23 +0300

    Готова авторизация

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
-from ui import Ui_MainWindow
-import Background_rc
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtCore, QtGui, QtWidgets
+from stage1 import Ui_MainWindow
+from AuthorizationWindow import Ui_AuthorizationWindow
 
-# Создать приложение
-app = QtGui.QGuiApplication(sys.argv)
+#app = QApplication([])
+#window = Window()
+#form = Form()
+#form.setupUi(window)
+#window.show()
 
-# Создать форму и инициализировать Ui
-Form = QtGui.QMainWindow()
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
-Form.show()
-# Логика приложения
+MainWindow.show()
+
+def Genres_Button():# Кнопка запуска окна выбора жанра
+    print("clicked!!!")
+ui.pushButton_3.clicked.connect(Genres_Button)
+def Search_Button():# Кнопка активации поиска
+    print("clicked!!!")
+    print(ui.plainTextEdit.toPlainText())
+ui.pushButton_7.clicked.connect(Search_Button)
+def Settings_Button():# Кнопка запуска окна с настройками
+    print("clicked!!!")
+ui.pushButton_5.clicked.connect(Settings_Button)
+
+def Authorization_Button_openOtherWindow(): #Кнопка открытия окна диалога
+    global AuthorizationWindow
+    AuthorizationWindow = QtWidgets.QDialog()
+    ui = Ui_AuthorizationWindow()
+    ui.setupUi(AuthorizationWindow)
+    MainWindow.close()
+    #Dialog.hide()
+    AuthorizationWindow.show()
+
+    def returnToMain(): #Кнопка возврата на основное окно
+        AuthorizationWindow.close()
+        MainWindow.show()
+    ui.BackButton.clicked.connect(returnToMain)
+
+ui.pushButton_6.clicked.connect(Authorization_Button_openOtherWindow)
 
 
-# Запустить Логику
-sys.exit(app.exec_())
+
+sys.exit(app.exec())
+
